05a00d1348d74272c7b9391531446e6da2ef3395
TaiseSilva <96255989+TaiseSilva@users.noreply.github.com>
2022-05-15 01:13:52 -0300

    Add files via upload

---
+++ Aula 03/estrutura_for-imprimir-1-20.c
@@ -0,0 +1,18 @@
+/*Faça um programa utilizando o comando for que imprima na 
+tela os números de 1 a 20, com a frase “Relembrando Pd1”.*/ 
+
+#include<stdio.h>
+
+
+int main(void){
+	
+	int  c;
+	
+	
+	for(c=1;c<=20;c++ ){
+		printf("%d - Relembrando PD1 \n",c);
+	}
+	
+system("Pause");	
+return 0;
+}
